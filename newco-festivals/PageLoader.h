a40892fb3c0e4453eb61f898ca5ea5a906a9f889
yass8n <yass8n@yahoo.com>
2016-02-11 14:49:54 -0800

    added pageloader, session cell header, and session cell view files

---
+++ newco-festivals/PageLoader.h
@@ -0,0 +1,13 @@
+//
+//  PageLoader.h
+//  newco-festivals
+//
+//  Created by yassen aniss 
+//  Copyright © 2016 newco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageLoader : UIView
+
+@end
---
+++ newco-festivals/PageLoader.m
@@ -0,0 +1,21 @@
+//
+//  PageLoader.m
+//  newco-festivals
+//
+//  Created by yassen aniss 
+//  Copyright © 2016 newco. All rights reserved.
+//
+
+#import "PageLoader.h"
+
+@implementation PageLoader
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
