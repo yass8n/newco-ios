46c1499f8765fa0a7ef9f9c35a46831aefa35065
Yaseen Anss <yaseen@Yaseens-MacBook-Pro.local>
2016-02-17 21:07:13 -0800

    Added firebase to app consts files

---
+++ newco-festivals/AppConst.h
@@ -0,0 +1,14 @@
+//
+//  AppConst.h
+//  newco-festivals
+//
+//  Created by Yaseen Anss on 2/17/16.
+//  Copyright © 2016 newco. All rights reserved.
+//
+
+#ifndef AppConst_h
+#define AppConst_h
+extern NSString* const firebaseUrl;
+
+
+#endif /* AppConst_h */
---
+++ newco-festivals/AppConst.m
@@ -0,0 +1,12 @@
+//
+//  AppConst.m
+//  newco-festivals
+//
+//  Created by Yaseen Anss on 2/17/16.
+//  Copyright © 2016 newco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AppConst.h"
+
+NSString* const kFirebaseUrl = @"https://newco-festivals.firebaseio.com"; //production
